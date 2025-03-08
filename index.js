feae35ead9fc1ac9aee8125f4c05f0ee9230906b
elvis <elvisotieno24@gmail.com>
2025-03-08 11:41:26 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+
+wrapGifts(gifts);
 function writeCards(names, event) {
-    let messages = []; 
-  
-    for (let i = 0; i < names.length; i++) {
-      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`; // Create the custom message
-      messages.push(message); 
-    }
-  
-    return messages; 
+  const messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
-  function countDown(num) {
-    while (num >= 0) {
-      console.log(num); 
-      num--; 
-    }
+
+  return messages; 
+}
+
+
+const result = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(result); 
+
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber); 
+    startingNumber--;
   }
-  
+}
+
+
+countDown(10);
